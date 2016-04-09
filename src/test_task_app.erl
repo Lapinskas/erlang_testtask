5a697d92a87fa446f238892b0515d701eb9ba861
Vladas Lapinskas <vlad.lapinskas@gmail.com>
2016-04-09 11:32:13 +0100

    Working gen server

---
+++ src/test_task_app.erl
@@ -0,0 +1,16 @@
+-module(test_task_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    test_task_sup:start_link().
+
+stop(_State) ->
+    ok.
